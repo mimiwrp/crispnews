8e533055b1db2788c256fbac03911bd722cf0161
mimiwrp <wirapa.boon@gmail.com>
2025-06-05 16:50:24 -0700

    fix play btn size

--- src/components/AudioPlayer.jsx
+++ src/components/AudioPlayer.jsx
@@ -82,15 +82,15 @@ const AudioPlayer = ({
         <div className="flex items-center space-x-4">
           <button 
             onClick={togglePlayPause}
-            className="btn flex items-center justify-center w-20 h-20 rounded-full hover:bg-gray-100 transition-colors"
+            className="btn flex items-center justify-center w-15 h-15 rounded-full transition-colors"
             title={isPlaying ? "Pause audio" : "Play audio"}
           >
             {isPlaying ? (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zM7 8a1 1 0 012 0v4a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v4a1 1 0 102 0V8a1 1 0 00-1-1z" clipRule="evenodd" />
               </svg>
             ) : (
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clipRule="evenodd" />
               </svg>
             )}
